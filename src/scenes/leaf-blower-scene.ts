792f0f7c770f4d30fe81e81b6ce89bc0694425c6
Markus Weskott <Martt@brain-force.intern>
2020-11-21 15:43:27 +0100

    cleanups

--- src/scenes/leaf-blower-scene.ts
+++ src/scenes/leaf-blower-scene.ts
@@ -1,7 +1,6 @@
 import * as Phaser from 'phaser';
-import { LeafBlower } from './leaf-blower';
 import { RoadSweeper } from './road-sweeper';
-import { Vector } from 'matter';
+import { LeafBlower } from './leaf-blower';
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
     active: false,
@@ -12,7 +11,6 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 export class LeafBlowerScene extends Phaser.Scene {
 
     private cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
-    private player: Phaser.Physics.Arcade.Sprite;
     private obstacles: Phaser.Physics.Arcade.StaticGroup;
 
     private leafs: Phaser.Physics.Arcade.Sprite[] = [];
@@ -23,15 +21,15 @@ export class LeafBlowerScene extends Phaser.Scene {
 
     private energy: number = 3000;
 
-    private blower: LeafBlower;
     private sweeper: RoadSweeper;
+    private leafBlower: LeafBlower;
 
 
     constructor() {
         super(sceneConfig);
 
-        this.blower = new LeafBlower(this, this.leafs);
         this.sweeper = new RoadSweeper(this, this.leafs);
+        this.leafBlower = new LeafBlower(this, this.leafs);
     }
 
     public preload(): void {
@@ -39,17 +37,15 @@ export class LeafBlowerScene extends Phaser.Scene {
         this.text = this.add.text(fontSize, fontSize, 'Starting...').setFontSize(fontSize).setDepth(100).setScrollFactor(0);
         this.sweeperText = this.add.text(0, 0, '').setFontSize(fontSize).setDepth(100);
 
-        this.load.image('player', 'assets/sprites/player.png');
         this.load.image('leaf', 'assets/sprites/leaf-4.png');
         this.load.image('background', 'assets/tiles/garden/garden-01-background.png');
         this.load.image('foreground', 'assets/tiles/garden/garden-01-foreground.png');
 
         this.load.spritesheet('leafs', 'assets/sprites/leaf-4.png', { frameWidth: 32, frameHeight: 32 });
 
-        this.load.audio('playerAh', 'assets/audio/player-ah.mp3');
 
-        this.blower.preload();
         this.sweeper.preload();
+        this.leafBlower.preload();
     }
 
     public create(): void {
@@ -77,24 +73,20 @@ export class LeafBlowerScene extends Phaser.Scene {
             this.leafs.push(leaf);
         }
 
-
-        this.player = this.physics.add.sprite(450, 400, 'player');
-        this.player.setCollideWorldBounds(true);
-        this.cameras.main.startFollow(this.player, true);
+        this.leafBlower.create();
+        this.cameras.main.startFollow(this.leafBlower.player, true);
 
         this.obstacles = this.physics.add.staticGroup();
         const obstacle1 = this.add.zone(432, 208, 32, 32);
         // this.add.rectangle(432, 208, 32, 32, 0x80ffffff);
         this.obstacles.add(obstacle1);
-        this.physics.add.collider(this.player, this.obstacles);
+        this.physics.add.collider(this.leafBlower.player, this.obstacles);
 
-        this.blower.player = this.player;
-        this.blower.create();
 
-        this.sweeper.player = this.player;
+        this.sweeper.player = this.leafBlower.player;
         this.sweeper.create();
         this.sweeper.onCollidePlayer = () => {
-            this.player.setPosition(this.player.x + 40, this.player.y);
+            this.leafBlower.player.setPosition(this.leafBlower.player.x + 40, this.leafBlower.player.y);
             this.playerAh.play();
             this.sweeperText.setPosition(this.sweeper.sweeper.x, this.sweeper.sweeper.y);
             this.sweeperText.setVisible(true);
@@ -140,19 +132,16 @@ export class LeafBlowerScene extends Phaser.Scene {
             playerNewVelocity.x = -100;
         }
 
-        playerNewVelocity.rotate(this.player.rotation);
-        this.player.setVelocity(playerNewVelocity.x, playerNewVelocity.y);    
-        this.player.setRotation(Phaser.Math.Angle.Wrap(this.player.rotation + playerNewRotation));
+        this.leafBlower.move(playerNewVelocity, playerNewRotation);
 
-        this.blower.isBlowing = false;
+        this.leafBlower.isBlowing = false;
         if (this.cursorKeys.space.isDown && this.energy > 0) {
             this.energy--;
-            this.blower.isBlowing = true;
+            this.leafBlower.isBlowing = true;
         }
 
 
-
-        this.blower.update();
+        this.leafBlower.update();
         this.sweeper.update();
 
         this.text.setText([
